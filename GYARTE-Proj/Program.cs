51e5455a4dd87abb47b62c418abde49109f0147d
Olle Järvheden <jarvheden.olle@elev.ga.ntig.se>
2022-08-19 09:08:37 +0200

    startingout

---
+++ GYARTE-Proj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GYARTE_Proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
